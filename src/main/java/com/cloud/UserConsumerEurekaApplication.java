baa3afc7245addbf7a0c587cca1163040baa9e6c
codingCrops <731249434@qq.com>
2018-01-16 16:59:09 +0800

    创建user-consumer-eureka

--- src/main/java/com/cloud/UserConsumerEurekaApplication.java
+++ src/main/java/com/cloud/UserConsumerEurekaApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class UserConsumerEurekaApplication{
 	public static void main(String[]args){
+		System.out.print("123");
 		SpringApplication.run(UserConsumerEurekaApplication.class, args);
 	}
 }
